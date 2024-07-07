8481051cd8469a9943e5c22014c432dbd5680f42
Trinaya Kantevari <108997403+trinaya-kantevari@users.noreply.github.com>
2024-07-07 16:12:15 +0530

    Added main.tf file

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.9.1"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Test_server"
+  }
+}
