2af4f0dab2957ebfce7e902f2736923273f07c3b
Trinaya Kantevari <108997403+trinaya-kantevari@users.noreply.github.com>
2024-07-07 20:03:09 +0530

    terraform script for provisioning backend resources.

--- backend/main.tf
+++ backend/main.tf
@@ -1,2 +1,33 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.9.1"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "ACCOUNT_ID-terraform-states"
+
+  tags = {
+    Name = "my terraform bucket"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
